ad8f96688da2cf4211c10ee0aaaa4753237fb374
orann11 <clippingsipping@gmail.com>
2026-01-03 01:05:57 +0300

    BarPanel is updated and Bar class is removed

--- src/main/java/Interface/BarPanel.java
+++ src/main/java/Interface/BarPanel.java
@@ -7,18 +7,26 @@ import java.util.ArrayList;
 public class BarPanel extends JPanel {
     private static final Color BACKGROUND_COLOR = Color.black;
     private static final Color BAR_COLOR = Color.white;
-
-    private ArrayList<Rectangle> bars = new ArrayList<>();
+    private static final Color CURRENT_BAR_COLOR = Color.green;
+    private static final int FIX_WIDTH = 10;
 
     public BarPanel(){}
 
 
 
-    public void createBars(int[] arr){
+    public void createBars(int[] arr, Graphics g){
+        int height, x, y;
+        x = 10;
         for(int number : arr){
-
-            bars.add(new Rectangle());
+            height = number * 10;
+            y = number;
+            g.drawRect(FIX_WIDTH, height, x, y);
+            x+= 10;
         }
+    }
+
+    public void clearPanel(){
+        this.removeAll();
     }
 
 
